d1201d5d4f0721934697f41ff41d85b3144539dc
HCPTangHY <tanghy06025@163.com>
2023-01-20 20:44:04 +0800

    modifier map.js&MapController.php

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -23,7 +23,38 @@ class MapController extends Controller {
     }
 
     public function changeOwner(Request $request){
-        $id=$request->input("id");
+        $uid = $request->session()->get('uid');
+        if(User::where("uid",$uid)->exists()) {
+            $privilege = User::where("uid", $uid)->first()->privilege;
+            if ($privilege == 0 || $privilege == 1) {
+                $id = $request->input("id");
+                $newOwner = $request->input("owner");
+                $lastOwner = Star::where(["id"=>$id])->first()->owner;
+                if ($lastOwner != '') {
+                    $lastCStars = json_decode(Country::where(["tag"=>$lastOwner])->first()->stars,true);
+                    foreach ($lastCStars as $key => $value) {
+                        if ($value == $id) {
+                            unset($lastCStars[$key]);
+                            array_values($lastCStars);
+                            break;
+                        }
+                    }
+                    $lastCStars = json_encode($lastCStars,JSON_UNESCAPED_UNICODE);
+                    Country::where(["tag"=>$lastOwner])->update(["stars"=>$lastCStars]);
+                }
+                Star::where(["id"=>$id])->update(["owner"=>$newOwner,"controller"=>$newOwner]);
+                $c = Country::where(["tag"=>$newOwner])->first();
+                $CStars = json_decode($c->stars,true);
+                $CStars[] = $id*1;
+                $CStars = json_encode($CStars,JSON_UNESCAPED_UNICODE);
+                Countries::where(["tag"=>$newOwner])->update(["stars"=>$CStars]);
+                echo $c->color;
+                return;
+            }
+        }else {
+            return redirect("https://kanade.nbmun.cn");
+        }
+
     }
 
 }
--- public/js/map.js
+++ public/js/map.js
@@ -2,7 +2,6 @@ function changeOwner(id,owner) {
     $.post('/Action/ChangeOwner',{
         id : id,
         owner : owner,
-        type : "changeOwner"
     },function(color) {
         $("#"+id+"MenuLink").css("background-color",color);
     })
